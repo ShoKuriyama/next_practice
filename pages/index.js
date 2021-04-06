1cd5024ba8cb44e0c79af5a5bf2649c56d8dbf07
shokuriyama <oranger04771@gmail.com>
2021-04-06 14:45:34 +0900

    made fetchAPI by useState

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import Layout from "../components/layout";
 import MyImage from "../components/image";
+import { useState } from "react";
 
 export default function Home() {
+  const url = "./data.json";
+  const [data, setData] = useState({ message: "", data: [] });
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => setData(res));
+
   return (
     <div>
       <Layout header="Next.js" title="Top page">
@@ -13,6 +20,27 @@ export default function Home() {
           </Link>
         </div>
         <MyImage fname="office.jpg" size="300px" />
+        <div className="alert alert-primary text-center">
+          <h5>{data.message}</h5>
+          <table className="table bg-white">
+            <thead className="table-dark">
+              <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <td>{value.name}</td>
+                  <td>{value.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </Layout>
     </div>
   );
